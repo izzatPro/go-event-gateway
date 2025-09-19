c0ca0ffb122bcf3c378d6b73fc1628d6959a4526
izzatPro <108723105+izzatPro@users.noreply.github.com>
2025-09-19 10:59:09 +0500

    feat(http): carved the inaugural GET into the gateway stone 🗿

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	server.GET("/events", getEvents) //get post put patch delete
+	server.Run(":8080")              //localhost : 8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello !"})
 }
