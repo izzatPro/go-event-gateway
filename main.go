7390c56d8eb98a56f44e9a271685327de6e66aca
izzatPro <108723105+izzatPro@users.noreply.github.com>
2025-09-21 11:31:11 +0500

    feat(db): integrate SQLite database and migrationsЭ

--- main.go
+++ main.go
@@ -4,14 +4,36 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/izzatPro/go-event-gateway/db"
+	"github.com/izzatPro/go-event-gateway/models"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 	server.GET("/events", getEvents) //get post put patch delete
-	server.Run(":8080")              //localhost : 8080
+	server.POST("/events", createEvent)
+	server.Run(":8080") //localhost : 8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello !"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created ! ", "event": event})
 }
